06d8ea451a5e9edded78e6fb599de1a8177308ee
angelica-maineri <angelica@odissei-data.nl>
2023-06-26 16:43:28 +0200

    Added html

---
+++ Scripts/data_cleaning.R
@@ -0,0 +1,30 @@
+#' ODISSEI code library
+#' Created by Angelica Maineri on 26-06-2023
+#' 
+#' 
+#' 
+
+
+# Load packages
+library(tidyverse)
+
+
+# Load data
+df = read_csv("C:/Users/angel/Documents/GitHub/ODISSEI-code-library/Data/odissei-projects.csv")
+
+#### Cleaning ------- 
+# add hyperlinks
+df2 = df |>
+  mutate(publication = paste("<a href=\"", publication, "\">", "doi</a>", sep = "")) |>
+  mutate(code = paste("<a href=\"", code, "\">", "link</a>", sep = "")) |>
+  mutate(grantee = paste("<a href=\"", orcid, "\">", grantee, "</a>", sep = ""))
+
+
+df2 = df2 |> 
+  select(-c("orcid"))
+
+head(df2)
+
+
+## Export ----
+write.csv(df2, "Data/odissei-projects-clean.csv")

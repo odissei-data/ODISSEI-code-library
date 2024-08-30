cf8fe41bad838628265512c533b9fbd86c245b9c
Angelica M. Maineri <82025706+AngelicaMaineri@users.noreply.github.com>
2024-08-30 16:18:16 +0200

    Added files

---
+++ data-prep/data_cleaning_CBS.R
@@ -0,0 +1,38 @@
+#' ODISSEI code library -- CBS
+#' Created by Angelica Maineri on 26-06-2023
+#' 
+#' To do:: add hyperlinks to data
+#' 
+
+
+# Load packages
+library(tidyverse)
+
+
+# Load data
+df = read.csv("C:/Users/angel/Documents/GitHub/odissei/ODISSEI-code-library/Data/odissei-projects_CBS.csv", encoding = "UTF-8")
+
+#### Cleaning ------- 
+# add hyperlinks
+df2 = df |>
+  mutate(publication = if_else(!is.na(publication), paste("<a href=\"", publication, "\">", "doi</a>", sep = ""), " ")) |>
+  mutate(project_lead = paste("<a href=\"", orcid, "\">", project_lead, "</a>", sep = "")) |>
+  mutate(title = paste("<a href=\"", code, "\">", title, "</a>", sep = ""))
+
+
+df2 = df2 |> 
+  select(-c("orcid", "code"))
+
+head(df2)
+
+colnames(df2) = c("Title", "CBS project nr.", "Project lead", "ODISSEI grant", "Publication", "Data", "Code Language")
+
+df2 = df2[,c(1,3,7,5,6,2,4)]
+
+df2$`Project lead` = gsub("<e8>", "è", df2$`Project lead`)
+
+## Export ----
+write.csv(df2, "Data/odissei-projects-clean_CBS.csv")
+
+
+
---
+++ data-prep/data_cleaning_LISS.R
@@ -0,0 +1,42 @@
+#' ODISSEI code library -- LISS
+#' Created by Angelica Maineri on 06-07-2023
+#' #' 
+#' 
+#' 
+
+
+# Load packages
+library(tidyverse)
+library(stringr)
+
+# Load data
+df = read.csv("C:/Users/angel/Documents/GitHub/odissei/ODISSEI-code-library/Data/odissei-projects_LISS.csv", encoding = "UTF-8")
+
+#### Cleaning ------- 
+# add hyperlinks
+df2 = df |>
+  mutate(publication = if_else(!is.na(publication), paste("<a href=\"", publication, "\">", "doi</a>", sep = ""), " ")) |>
+  mutate(project_lead = paste("<a href=\"", orcid, "\">", project_lead, "</a>", sep = "")) |>
+  mutate(title = paste("<a href=\"", code, "\">", title, "</a>", sep = ""))
+
+
+df2$link_data = gsub('https://','<a href="https://', df2$link_data )
+df2$link_data = gsub('*$','">data</a>', df2$link_data )
+df2$link_data = gsub(';','">data</a>;', df2$link_data )
+df2$link_data[df2$link_data=='\">data</a>'] = ""
+
+df2$link_data
+df2$project_lead
+
+
+
+df2 = df2 |> 
+  select(-c("orcid", "publication.type", "code"))
+
+head(df2)
+
+colnames(df2) = c("Title", "Project lead", "ODISSEI grant", "Publication", "Data", "Link to data", "Code Language")
+df2 = df2[,c(1,2,7,4,5,6,3)]
+
+## Export ----
+write.csv(df2, "Data/odissei-projects-clean_LISS.csv")
